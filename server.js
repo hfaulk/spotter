f771e28f9ae786def4ed0e5cb54587679ffa236e
Harry <hfaulkner2006@gmail.com>
2026-06-20 14:53:00 +0100

    Load RESEND_API_KEY in server.js from .env

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const REQUIRED_ENV = [
   "R2_SECRET_ACCESS_KEY",
   "R2_BUCKET_NAME",
   "R2_PUBLIC_URL",
+  "RESEND_API_KEY",
 ];
 const missingEnv = REQUIRED_ENV.filter((k) => !process.env[k]);
 if (missingEnv.length) {
